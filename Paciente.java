d79c2028d49970efb2eb425e233ab18bf4c3cef1
MartinVillatoros <martinguille17@gmail.com>
2025-04-10 10:55:30 -0600

    Clase paciente para manejar sus atributos

---
+++ Paciente.java
@@ -0,0 +1,28 @@
+public class Paciente implements Comparable<Paciente> {
+    private String nombre;
+    private String sintoma;
+    private char prioridad;
+
+    public Paciente(String nombre, String sintoma, char prioridad) {
+        this.nombre = nombre;
+        this.sintoma = sintoma;
+        this.prioridad = prioridad;
+    }
+
+    public String getNombre() {return nombre;}
+
+    public String getSintoma() {return sintoma;}
+
+    public char getPrioridad() {return prioridad;}
+
+    @Override
+    public int compareTo(Paciente otro) {
+        // Compara  prioridades: A es más urgente que B, etc.
+        return Character.compare(this.prioridad, otro.prioridad);
+    }
+
+    @Override
+    public String toString() {
+        return nombre + ", " + sintoma + ", " + prioridad;
+    }
+}
